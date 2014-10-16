a893b5520e39f426aacde19ba71e08f966566cbb
Frederick Polgardy <fred@polgardy.com>
2014-10-16 10:16:07 -0500

    move array get/set into the array object

--- start-lib.js
+++ start-lib.js
@@ -2,24 +2,76 @@ module.exports = (function() {
 
   var slice = [].slice;
 
-  return {
-    array: function array(first) {
-      if (first == null) {
-        return;
+  function SArray(dimensions) {
+    if (dimensions.length == 0) {
+      throw new Error('array must have at least one dimension');
+    }
+
+    this._array = buildSubArray(dimensions);
+    this._dimensions = dimensions;
+  }
+
+  function buildSubArray(dimensions) {
+    var sub = new Array(dimensions[0]), i;
+
+    if (dimensions[0] == 0) {
+      throw new Error('array cannot have a zero-length dimension');
+    }
+
+    if (dimensions.length > 1) {
+      for (i = 0; i < dimensions[0]; ++i) {
+        sub[i] = buildSubArray(dimensions.slice(1));
       }
+    }
 
-      var ary = new Array(first),
-          rest = slice.call(arguments, 1),
-          i;
+    return sub;
+  }
 
-      if (arguments.length > 1) {
-        for (i = 0; i < first; ++i) {
-          ary[i] = array.apply(this, rest);
+  SArray.prototype.get = function(indexes) {
+    if (this._dimensions.length != indexes.length) {
+      throw new Error('array index has wrong number of dimensions');
+    }
+
+    var current = this._array, index, i;
+
+    for (i = 0; i < indexes.length; ++i) {
+      index = indexes[i];
+
+      if (index < 0 || index >= current.length) {
+        throw new Error('array index out of bounds');
+      } else {
+        current = current[index];
+      }
+    }
+
+    return current;
+  };
+
+  SArray.prototype.set = function(indexes, value) {
+    if (this._dimensions.length != indexes.length) {
+      throw new Error('array index has wrong number of dimensions');
+    }
+
+    var current = this._array, index, i;
+
+    for (i = 0; i < indexes.length; ++i) {
+      index = indexes[i];
+
+      if (index < 0 || index >= current.length) {
+        throw new Error('array index out of bounds');
+      } else {
+        if (i < indexes.length - 1) {
+          current = current[index];
+        } else {
+          current[index] = value;
         }
       }
+    }
+  };
 
-      ary._dimensions = arguments.length;
-      return ary;
+  return {
+    array: function() {
+      return new SArray(slice.apply(arguments));
     }
   };
 })();
