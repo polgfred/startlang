7985571aa11b700fe374dcdcd57e776f909b1457
Frederick Polgardy <fred@polgardy.com>
2014-10-16 23:34:37 -0500

    pass an execution context to handle all variable values

--- start-lib.js
+++ start-lib.js
@@ -1,6 +1,48 @@
 module.exports = (function() {
 
-  var slice = [].slice;
+  var startlib,
+      slice = [].slice;
+
+  function mixin(object, properties) {
+    Object.keys(properties).forEach(function(prop) {
+      object[prop] = properties[prop];
+    });
+  }
+
+  // Environment
+
+  function SEnvironment() {
+    this.namespace = {};
+  }
+
+  // push and pop new objects onto the prototype chain to implement fast scopes
+  mixin(SEnvironment.prototype, {
+    push: function() {
+      this.namespace = Object.create(this.namespace);
+    },
+
+    pop: function() {
+      this.namespace = Object.getPrototypeOf(this.namespace);
+    },
+
+    get: function(name) {
+      return startlib[name] || this.namespace[name];
+    },
+
+    getIndex: function(name, indexes) {
+      return this.get(name).get(indexes);
+    },
+
+    set: function(name, value) {
+      this.namespace[name] = value;
+    },
+
+    setIndex: function(name, indexes, value) {
+      this.get(name).set(indexes, value);
+    }
+  });
+
+  // Multi-dimensional arrays
 
   function SArray(dimensions) {
     if (dimensions.length == 0) {
@@ -27,51 +69,61 @@ module.exports = (function() {
     return sub;
   }
 
-  SArray.prototype.get = function(indexes) {
-    if (this._dimensions.length != indexes.length) {
-      throw new Error('array index has wrong number of dimensions');
-    }
-
-    var current = this._array, index, i;
-
-    for (i = 0; i < indexes.length; ++i) {
-      index = indexes[i];
-
-      if (index < 0 || index >= current.length) {
-        throw new Error('array index out of bounds');
-      } else {
-        current = current[index];
+  mixin(SArray.prototype, {
+    get: function(indexes) {
+      if (this._dimensions.length != indexes.length) {
+        throw new Error('array index has wrong number of dimensions');
       }
-    }
 
-    return current;
-  };
+      var current = this._array, index, i;
 
-  SArray.prototype.set = function(indexes, value) {
-    if (this._dimensions.length != indexes.length) {
-      throw new Error('array index has wrong number of dimensions');
-    }
+      for (i = 0; i < indexes.length; ++i) {
+        index = indexes[i];
 
-    var current = this._array, index, i;
-
-    for (i = 0; i < indexes.length; ++i) {
-      index = indexes[i];
-
-      if (index < 0 || index >= current.length) {
-        throw new Error('array index out of bounds');
-      } else {
-        if (i < indexes.length - 1) {
-          current = current[index];
+        if (index < 0 || index >= current.length) {
+          throw new Error('array index out of bounds');
         } else {
-          current[index] = value;
+          current = current[index];
+        }
+      }
+
+      return current;
+    },
+
+    set: function(indexes, value) {
+      if (this._dimensions.length != indexes.length) {
+        throw new Error('array index has wrong number of dimensions');
+      }
+
+      var current = this._array, index, i;
+
+      for (i = 0; i < indexes.length; ++i) {
+        index = indexes[i];
+
+        if (index < 0 || index >= current.length) {
+          throw new Error('array index out of bounds');
+        } else {
+          if (i < indexes.length - 1) {
+            current = current[index];
+          } else {
+            current[index] = value;
+          }
         }
       }
     }
-  };
+  });
 
-  return {
+  return startlib = {
+    createEnv: function() {
+      return new SEnvironment();
+    },
+
+    createCallable: function(func) {
+      return { call: func };
+    },
+
     array: {
-      run: function(args) {
+      call: function(ctx, args) {
         return new SArray(args);
       }
     }
