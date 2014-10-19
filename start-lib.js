0d8a64fb96190a9996630cf84011eb51a5a6037f
Frederick Polgardy <fred@polgardy.com>
2014-10-19 13:00:34 -0500

    move all the operator logic into type handlers

--- start-lib.js
+++ start-lib.js
@@ -17,11 +17,6 @@ module.exports = (function() {
       return { call: func };
     },
 
-    // find a protocol handler for this object
-    handle: function(obj) {
-      return obj._handler || _snumber.handle(obj) || _sstring.handle(obj);
-    },
-
     // push and pop new objects onto the prototype chain to implement fast scopes
     push: function() {
       this._ns = Object.create(this._ns);
@@ -39,32 +34,68 @@ module.exports = (function() {
       this._ns[name] = value;
     },
 
+    unaryOp: function(op, right) {
+      return _handle(right).unaryOp(op, right);
+    },
+
+    binaryOp: function(op, left, right) {
+      return _handle(left).binaryOp(op, left, right);
+    },
+
     getIndex: function(base, index) {
-      var handler = this.handle(base);
-
-      if (!handler || !handler.getIndex) {
-        throw new Error('object does not support getting by index');
-      }
-
-      return handler.getIndex(base, index);
+      return _handle(base).getIndex(base, index);
     },
 
     setIndex: function(base, index, value) {
-      var handler = this.handle(base);
-
-      if (!handler || !handler.setIndex) {
-        throw new Error('object does not support setting by index');
-      }
-
-      handler.setIndex(base, index, value);
+      _handle(base).setIndex(base, index, value);
     }
   });
+
+  // find a protocol handler for this object
+  function _handle(obj) {
+    return obj['$$handler$$'] || _snumber.handle(obj) || _sstring.handle(obj);
+  }
 
   var _snumber = {
     handle: function(n) {
       if (typeof n == 'number') {
         return _snumber;
       }
+    },
+
+    unaryOp: function(op, right) {
+      return this.unaryImpl[op](right);
+    },
+
+    binaryOp: function(op, left, right) {
+      return this.binaryImpl[op](left, right);
+    },
+
+    getIndex: function(s, index) {
+      throw new Error('object does not support []');
+    },
+
+    setIndex: function(a, index, value) {
+      throw new Error('object does not support [] assignment');
+    },
+
+    unaryImpl: {
+      '+' : function(right) { return + right; },
+      '-' : function(right) { return - right; }
+    },
+
+    binaryImpl: {
+      '+' : function(left, right) { return left +  right; },
+      '-' : function(left, right) { return left -  right; },
+      '*' : function(left, right) { return left *  right; },
+      '/' : function(left, right) { return left /  right; },
+      '%' : function(left, right) { return left %  right; },
+      '=' : function(left, right) { return left == right; },
+      '!=': function(left, right) { return left != right; },
+      '<' : function(left, right) { return left <  right; },
+      '<=': function(left, right) { return left <= right; },
+      '>' : function(left, right) { return left >  right; },
+      '>=': function(left, right) { return left >= right; }
     }
   };
 
@@ -75,8 +106,30 @@ module.exports = (function() {
       }
     },
 
+    unaryOp: function(op, right) {
+      throw new Error('object does not support unary ' + op);
+    },
+
+    binaryOp: function(op, left, right) {
+      return this.binaryImpl[op](left, right);
+    },
+
     getIndex: function(s, index) {
       return s.charAt(index);
+    },
+
+    setIndex: function(a, index, value) {
+      throw new Error('object does not support [] assignment');
+    },
+
+    binaryImpl: {
+      '+' : function(left, right) { return left +  right; },
+      '=' : function(left, right) { return left == right; },
+      '!=': function(left, right) { return left != right; },
+      '<' : function(left, right) { return left <  right; },
+      '<=': function(left, right) { return left <= right; },
+      '>' : function(left, right) { return left >  right; },
+      '>=': function(left, right) { return left >= right; }
     }
   };
 
@@ -88,21 +141,30 @@ module.exports = (function() {
         dims.push(0);
       }
 
-      var a = this._buildSubArray(dims);
-      return a;
+      return this._buildSubArray(dims);
     },
 
     _buildSubArray: function(dims) {
-      var sub = new Array(dims[0]);
-      sub._handler = _sarray;
+      var sub = new Array(dims[0]),
+          next = dims.slice(1);
+
+      sub['$$handler$$'] = _sarray;
 
       if (dims.length > 1) {
         for (var i = 0; i < dims[0]; ++i) {
-          sub[i] = this._buildSubArray(dims.slice(1));
+          sub[i] = this._buildSubArray(next);
         }
       }
 
       return sub;
+    },
+
+    unaryOp: function(op, right) {
+      throw new Error('object does not support unary ' + op);
+    },
+
+    binaryOp: function(op, left, right) {
+      return this.binaryImpl[op](left, right);
     },
 
     getIndex: function(a, index) {
@@ -111,6 +173,24 @@ module.exports = (function() {
 
     setIndex: function(a, index, value) {
       a[index] = value;
+    },
+
+    binaryImpl: {
+      '=' : function(left, right) {
+        // arrays have the same length and all their items are equal
+        return (left.length == right.length) && left.every(function(litem, i) {
+          return _handle(litem).binaryOp('=', litem, right[i]);
+        });
+      },
+
+      '!=': function(left, right) {
+        return ! this['='](left, right);
+      },
+
+      '<' : function(left, right) { return left <  right; },
+      '<=': function(left, right) { return left <= right; },
+      '>' : function(left, right) { return left >  right; },
+      '>=': function(left, right) { return left >= right; }
     }
   };
 
@@ -118,9 +198,16 @@ module.exports = (function() {
 
   var _stable = {
     create: function() {
-      var t = {};
-      t._handler = _stable;
-      return t;
+      return { '$$handler$$': _stable };
+    },
+
+
+    unaryOp: function(op, right) {
+      throw new Error('object does not support unary ' + op);
+    },
+
+    binaryOp: function(op, left, right) {
+      return this.binaryImpl[op](left, right);
     },
 
     getIndex: function(t, index) {
@@ -129,6 +216,9 @@ module.exports = (function() {
 
     setIndex: function(t, index, value) {
       t[index] = value;
+    },
+
+    binaryImpl: {
     }
   };
 
@@ -147,7 +237,9 @@ module.exports = (function() {
       call: function(ctx, args) {
         return _stable.create(args);
       }
-    }
+    },
+
+    '$$handle$$': _handle
   };
 
   return startlib;
