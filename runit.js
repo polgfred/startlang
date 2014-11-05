5f8290224f81258b87ced37d2704cfa68550a4ac
Frederick Polgardy <fred@polgardy.com>
2014-11-04 19:53:08 -0600

    resurrect the node.js runner

---
+++ runit.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+    util = require('util'),
+    requirejs = require('requirejs');
+
+requirejs.config({
+  baseUrl: 'js',
+  nodeRequire: require
+});
+
+requirejs(['start-lang', 'start-lib'], function(startlang, startlib) {
+  var ctx = startlib.createEnv(), code, prog;
+
+  try {
+    code = fs.readFileSync(process.argv[2], 'utf-8');
+  } catch (e) {
+    code = process.argv[2] + '\n';
+  }
+
+  try {
+    prog = startlang.parse(code);
+    util.puts(util.inspect(prog, false, null));
+    prog.run(ctx);
+  } catch (e) {
+    console.log(util.inspect(e, false, null));
+    throw e;
+  } finally {
+    console.log(util.inspect(ctx, false, null));
+  }
+});
