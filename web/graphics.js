f08a9ca52e80673fc5b87a2079f4e42cc8078741
Frederick Polgardy <fred@polgardy.com>
2015-03-07 20:32:29 -0600

    generic shape methods

--- web/graphics.js
+++ web/graphics.js
@@ -2,59 +2,140 @@ var util = require('util'),
     runtime = require('../runtime.js'),
     Snap = require('snapsvg');
 
+function applyTransforms(el) {
+  var rot = el.data('rot'),
+      skew = el.data('skew'),
+      mat = new Snap.Matrix();
+
+  if (rot || skew) {
+    var bb = el.getBBox(true); // without transforms
+
+    if (rot) {
+      // rotate from the center of the object
+      mat.rotate(rot, bb.cx, bb.cy);
+    }
+
+    if (skew) {
+      // apply the scaling factor
+      mat.scale(skew[0], skew[1], bb.cx, bb.cy);
+    }
+  }
+
+  el.transform(mat);
+}
+
 var SShape = exports.SShape = {
   // keep a reference to the canvas element
   paper: Snap('#canvas'),
 
-  getindex: function(el, index) {
-    return el.attr(index);
+  repr: function(el) {
+    return '*' + el.type + ':' + el.id + '*';
   },
 
-  setindex: function(el, index, value) {
-    el.attr(index, value);
-    return el;
-  },
+  methods: {
+    fill: function(el, color) {
+      el.attr('fill', color);
+    },
 
-  methods: {}
-};
+    stroke: function(el, color) {
+      el.attr('stroke', color);
+    },
 
-var SCircle = exports.SShape = {};
-util._extend(SCircle, SShape);
-util._extend(SCircle, {
-  repr: function(circ) {
-    return '*circle(' + circ.id + ')*';
-  },
+    opacity: function(el, opac) {
+      if (opac == null || opac == 1) {
+        el.attr('opacity', null);
+      } else {
+        el.attr('opacity', opac);
+      }
+    },
 
-  methods: {}
-});
+    rotate: function(el, rot) {
+      if (rot == null || rot == 0) {
+        el.removeData('rot');
+      } else {
+        el.data('rot', rot);
+      }
 
-util._extend(SCircle.methods, SShape.methods);
-util._extend(SCircle.methods, {
-  moveto: function(el, x, y) {
-    el.attr({ cx: x, cy: y });
+      applyTransforms(el);
+    },
+
+    scale: function(el, sx, sy) {
+      // default sy to sx
+      if (sy == null) {
+        sy = sx;
+      }
+
+      if (sx == null || (sx == 1 && sy == 1)) {
+        el.removeData('skew');
+      } else {
+        el.data('skew', [sx, sy]);
+      }
+
+      applyTransforms(el);
+    },
+
+    clone: function(el) {
+      var el2 = el.clone();
+      // copy transform data
+      el2.data(el.data());
+      return el2;
+    },
+
+    remove: function(el) {
+      el.remove();
+    }
   }
-});
+};
 
 var SRect = exports.SShape = {};
 util._extend(SRect, SShape);
 util._extend(SRect, {
-  repr: function(rect) {
-    return '*rect(' + rect.id + ')*';
-  },
-
   methods: {}
 });
 
 util._extend(SRect.methods, SShape.methods);
 util._extend(SRect.methods, {
-  moveto: function(el, x, y) {
+  move: function(el, x, y) {
     el.attr({ x: x, y: y });
+
+    applyTransforms(el);
+  }
+});
+
+var SCircle = exports.SShape = {};
+util._extend(SCircle, SShape);
+util._extend(SCircle, {
+  methods: {}
+});
+
+util._extend(SCircle.methods, SShape.methods);
+util._extend(SCircle.methods, {
+  move: function(el, x, y) {
+    el.attr({ cx: x, cy: y });
+
+    applyTransforms(el);
+  }
+});
+
+var SEllipse = exports.SShape = {};
+util._extend(SEllipse, SShape);
+util._extend(SEllipse, {
+  methods: {}
+});
+
+util._extend(SEllipse.methods, SShape.methods);
+util._extend(SEllipse.methods, {
+  move: function(el, x, y) {
+    el.attr({ cx: x, cy: y });
+
+    applyTransforms(el);
   }
 });
 
 Snap.plugin(function(_, Element, Paper) {
   var handlerMap = {
     circle: SCircle,
+    ellipse: SEllipse,
     rect: SRect
   };
 
@@ -69,6 +150,10 @@ Snap.plugin(function(_, Element, Paper) {
 util._extend(runtime.globals, {
   circle: function(x, y, r) {
     return SShape.paper.circle(x, y, r);
+  },
+
+  ellipse: function(x, y, rx, ry) {
+    return SShape.paper.ellipse(x, y, rx, ry);
   },
 
   rect: function(x, y, w, h) {
