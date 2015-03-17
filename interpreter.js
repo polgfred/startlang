786b6f55083ea9b79fb680a3e00df95c979f2182
Frederick Polgardy <fred@polgardy.com>
2015-03-17 09:42:19 -0500

    convert interpreter to es6!

--- interpreter.js
+++ interpreter.js
@@ -1,49 +1,41 @@
-var util = require('util'),
-    events = require('events'),
+var events = require('events'),
     // cache this for performance
     hasOwnProperty = Object.prototype.hasOwnProperty,
     // shared control object for the enter event
     control = {};
 
-function ScriptExit() {}
+export class ScriptExit extends Error {}
 
-util.inherits(ScriptExit, Error);
+export class SInterpreter extends events.EventEmitter {
+  constructor(root, ctx) {
+    this.root = root;
+    this.ctx = ctx;
+    this.frames = [];
+  }
 
-var SInterpreter = exports.SInterpreter = function(root, ctx) {
-  this.root = root;
-  this.ctx = ctx;
-  this.frames = [];
-};
-
-util.inherits(SInterpreter, events.EventEmitter);
-
-util._extend(SInterpreter.prototype, {
-  // main entry point
-  run: function() {
-    var _this = this;
-    return _this.visit(_this.root).then(function() {
-      _this.emit('end');
-    }).catch(function(err) {
+  run() {
+    return this.visit(this.root).then(() => {
+      this.emit('end');
+    }).catch((err) => {
       if (err instanceof ScriptExit) {
-        _this.emit('end');
+        this.emit('end');
       } else {
-        _this.emit('error', err);
+        this.emit('error', err);
       }
     });
-  },
+  }
 
   // main node visitor
-  visit: function(node) {
+  visit(node) {
     // optimize literals: skip conversion, frames, events, and error handling,
     // and extract the value directly from the node without a function call
     if (node.type == 'literal') {
       return Promise.resolve({ rv: node.value });
     }
-    var _this = this;
     // push a frame onto the stack
-    _this.frames.push({ node: node, ns: _this.ctx.ns, stack: _this.ctx.stack });
+    //this.frames.push({ node: node, ns: this.ctx.ns, stack: this.ctx.stack });
     // give the caller a chance to exit or pause
-    _this.emit('enter', node, control);
+    this.emit('enter', node, control);
     if (control.exit) {
       control = {};
       // return a special error to exit the program
@@ -51,65 +43,62 @@ util._extend(SInterpreter.prototype, {
     } else if (control.pause) {
       control = {};
       // return a promise to resume execution when resume() is called
-      return new Promise(function(resolve) {
-        _this.resume = function() {
-          _this.resume = null;
+      return new Promise((resolve) => {
+        this.resume = () => {
+          this.resume = null;
           return this.nodeResult(node);
         };
       });
     }
     // return the node's result immediately
     return this.nodeResult(node);
-  },
+  }
 
   // safely get and normalize the node's result, and handle errors
-  nodeResult: function(node) {
-    var _this = this, method = node.type + 'Node';
-    return new Promise(function(resolve) {
-      resolve(_this[method](node));
-    }).then(function(result) {
+  nodeResult(node) {
+    var method = node.type + 'Node';
+    return new Promise((resolve) => {
+      resolve(this[method](node));
+    }).then((result) => {
       // if the node returns a plain value, convert it to an rvalue
       if (result == null || !hasOwnProperty.call(result, 'rv')) {
         result = { rv: result };
       }
       // notify caller that we're exiting
-      _this.emit('exit', node, result);
+      this.emit('exit', node, result);
       return result;
-    }).catch(function(err) {
+    }).catch((err) => {
       // attach the node where the error occured
       if (!err.node) {
         err.node = node;
       }
       throw err;
     });
-  },
+  }
 
   // ** implementations of AST nodes **
 
-  blockNode: function(node) {
-    var _this = this, len = node.elems.length;
-    return loop(0);
+  blockNode(node) {
+    var len = node.elems.length;
     // recursive loop over block statements
-    function loop(count) {
+    var loop = (count) => {
       if (count < len) {
-        return _this.visit(node.elems[count]).then(function(eres) {
+        return this.visit(node.elems[count]).then((eres) => {
           // propagate break/next/return
           return eres.flow ? eres : loop(count + 1);
         });
       }
     }
-  },
+    return loop(0);
+  }
 
-  forNode: function(node) {
-    var _this = this;
-    return _this.visit(node.range).then(function(rres) {
-      // convert the range to an enumeration
-      return loop(_this.ctx.enumerate(rres.rv));
+  forNode(node) {
+    return this.visit(node.range).then((rres) => {
       // recursive loop over range
-      function loop(iter) {
+      var loop = (iter) => {
         if (iter.more) {
-          _this.ctx.set(node.name, iter.value);
-          return _this.visit(node.body).then(function(bres) {
+          this.ctx.set(node.name, iter.value);
+          return this.visit(node.body).then((bres) => {
             var flow = bres.flow;
             if (flow == 'return') {
               return bres; // propagate
@@ -119,17 +108,17 @@ util._extend(SInterpreter.prototype, {
           });
         }
       }
+      // convert the range to an enumeration
+      return loop(this.ctx.enumerate(rres.rv));
     });
-  },
+  }
 
-  whileNode: function(node) {
-    var _this = this;
-    return loop();
+  whileNode(node) {
     // recursive loop while condition is true
-    function loop() {
-      return _this.visit(node.cond).then(function(cres) {
+    var loop = () => {
+      return this.visit(node.cond).then((cres) => {
         if (cres.rv) {
-          return _this.visit(node.body).then(function(bres) {
+          return this.visit(node.body).then((bres) => {
             var flow = bres.flow;
             if (flow == 'return') {
               return bres; // propagate
@@ -140,204 +129,195 @@ util._extend(SInterpreter.prototype, {
         }
       });
     }
-  },
+    return loop();
+  }
 
-  ifNode: function(node) {
-    var _this = this;
-    return _this.visit(node.cond).then(function(cres) {
+  ifNode(node) {
+    return this.visit(node.cond).then((cres) => {
       if (cres.rv) {
-        return _this.visit(node.tbody);
+        return this.visit(node.tbody);
       } else if (node.fbody) {
-        return _this.visit(node.fbody);
+        return this.visit(node.fbody);
       }
     });
-  },
+  }
 
-  beginNode: function(node) {
-    var _this = this, len = node.params ? node.params.length : 0;
-    return _this.ctx.setfn(node.name, fn);
+  beginNode(node) {
+    var len = node.params ? node.params.length : 0;
     // implement function body (invoked from call node)
-    function fn(args) {
+    var fn = (args) => {
       // push a new stack and set parameter values
-      _this.ctx.push();
+      this.ctx.push();
       for (var i = 0; i < len; ++i) {
-        _this.ctx.set(node.params[i], args[i]);
+        this.ctx.set(node.params[i], args[i]);
       }
       // capture a possible return value and then clean up
-      return _this.visit(node.body).then(function(bres) {
-        _this.ctx.pop();
+      return this.visit(node.body).then((bres) => {
+        this.ctx.pop();
         if (bres.flow == 'return') {
           return bres.rv;
         }
-      }, function(err) {
-        _this.ctx.pop();
+      }, (err) => {
+        this.ctx.pop();
         throw err;
       });
     }
-  },
+    return this.ctx.setfn(node.name, fn);
+  }
 
-  callNode: function(node) {
-    var _this = this, len = node.args ? node.args.length : 0, args = [], assn = [], fn;
-    return loop(0);
+  callNode(node) {
+    var len = node.args ? node.args.length : 0, args = [], assn = [], fn;
     // loop to collect arguments and call the function
-    function loop(count) {
+    var loop = (count) => {
       if (count == len) {
-        fn = _this.ctx.getfn(node.name);
-        return fn ? fn(args) : _this.ctx.syscall(node.name, args, assn);
+        fn = this.ctx.getfn(node.name);
+        return fn ? fn(args) : this.ctx.syscall(node.name, args, assn);
       } else {
-        return _this.visit(node.args[count]).then(function(ares) {
+        return this.visit(node.args[count]).then((ares) => {
           args[count] = ares.rv;
           assn[count] = ares.lv;
           return loop(count + 1);
         });
       }
     }
-  },
+    return loop(0);
+  }
 
-  breakNode: function() {
+  breakNode() {
     return { rv: undefined, flow: 'break' };
-  },
+  }
 
-  nextNode: function() {
+  nextNode() {
     return { rv: undefined, flow: 'next' };
-  },
+  }
 
-  returnNode: function(node) {
-    var _this = this;
+  returnNode(node) {
     if (node.result) {
-      return _this.visit(node.result).then(function(rres) {
+      return this.visit(node.result).then((rres) => {
         return { rv: rres.rv, flow: 'return' };
       });
     } else {
       return { rv: undefined, flow: 'return' };
     }
-  },
+  }
 
-  varNode: function(node) {
-    var _this = this;
-    return { rv: _this.ctx.get(node.name), lv: { name: node.name } };
-  },
+  varNode(node) {
+    return { rv: this.ctx.get(node.name), lv: { name: node.name } };
+  }
 
-  letNode: function(node) {
-    var _this = this;
-    return _this.visit(node.value).then(function(vres) {
-      return _this.ctx.set(node.name, vres.rv);
+  letNode(node) {
+    return this.visit(node.value).then((vres) => {
+      return this.ctx.set(node.name, vres.rv);
     });
-  },
+  }
 
-  deleteNode: function(node) {
-    var _this = this;
-    return _this.ctx.del(node.name);
-  },
+  deleteNode(node) {
+    return this.ctx.del(node.name);
+  }
 
-  indexNode: function(node) {
-    var _this = this, len = node.indexes.length, indexes = [];
-    return loop(0);
+  indexNode(node) {
+    var len = node.indexes.length, indexes = [];
     // collect indexes and lookup value
-    function loop(count) {
+    var loop = (count) => {
       if (count == len) {
         return {
-          rv: _this.ctx.getindex(node.name, indexes),
+          rv: this.ctx.getindex(node.name, indexes),
           lv: { name: node.name, indexes: indexes }
         };
       } else {
-        return _this.visit(node.indexes[count]).then(function(ires) {
+        return this.visit(node.indexes[count]).then((ires) => {
           indexes[count] = ires.rv;
           return loop(count + 1);
         });
       }
     }
-  },
-
-  letIndexNode: function(node) {
-    var _this = this, len = node.indexes.length, indexes = [];
     return loop(0);
+  }
+
+  letIndexNode(node) {
+    var len = node.indexes.length, indexes = [];
     // collect indexes and set value
-    function loop(count) {
+    var loop = (count) => {
       if (count == len) {
-        return _this.visit(node.value).then(function(vres) {
-          return _this.ctx.setindex(node.name, indexes, vres.rv);
+        return this.visit(node.value).then((vres) => {
+          return this.ctx.setindex(node.name, indexes, vres.rv);
         });
       } else {
-        return _this.visit(node.indexes[count]).then(function(ires) {
+        return this.visit(node.indexes[count]).then((ires) => {
           indexes[count] = ires.rv;
           return loop(count + 1);
         });
       }
     }
-  },
-
-  deleteIndexNode: function(node) {
-    var _this = this, len = node.indexes.length, indexes = [];
     return loop(0);
+  }
+
+  deleteIndexNode(node) {
+    var len = node.indexes.length, indexes = [];
     // collect indexes and delete value
-    function loop(count) {
+    var loop = (count) => {
       if (count == len) {
-        return _this.ctx.delindex(node.name, indexes);
+        return this.ctx.delindex(node.name, indexes);
       } else {
-        return _this.visit(node.indexes[count]).then(function(ires) {
+        return this.visit(node.indexes[count]).then((ires) => {
           indexes[count] = ires.rv;
           return loop(count + 1);
         });
       }
     }
-  },
+    return loop(0);
+  }
 
-  logicalOpNode: function(node) {
+  logicalOpNode(node) {
     var method = 'logicalOpNode_' + node.op;
     return this[method](node);
-  },
+  }
 
-  logicalOpNode_and: function(node) {
-    var _this = this;
-    return _this.visit(node.left).then(function(lres) {
+  logicalOpNode_and(node) {
+    return this.visit(node.left).then((lres) => {
       if (!lres.rv) {
         return false;
       } else {
-        return _this.visit(node.right).then(function(rres) {
+        return this.visit(node.right).then((rres) => {
           return !!rres.rv;
         });
       }
     });
-  },
+  }
 
-  logicalOpNode_or: function(node) {
-    var _this = this;
-    return _this.visit(node.left).then(function(lres) {
+  logicalOpNode_or(node) {
+    return this.visit(node.left).then((lres) => {
       if (lres.rv) {
         return true;
       } else {
-        return _this.visit(node.right).then(function(rres) {
+        return this.visit(node.right).then((rres) => {
           return !!rres.rv;
         });
       }
     });
-  },
+  }
 
-  logicalOpNode_not: function(node) {
-    var _this = this;
-    return _this.visit(node.right).then(function(rres) {
+  logicalOpNode_not(node) {
+    return this.visit(node.right).then((rres) => {
       return !rres.rv;
     });
-  },
+  }
 
-  binaryOpNode: function(node) {
-    var _this = this;
-    return _this.visit(node.left).then(function(lres) {
-      return _this.visit(node.right).then(function(rres) {
-        return _this.ctx.binaryop(node.op, lres.rv, rres.rv);
+  binaryOpNode(node) {
+    return this.visit(node.left).then((lres) => {
+      return this.visit(node.right).then((rres) => {
+        return this.ctx.binaryop(node.op, lres.rv, rres.rv);
       });
     });
-  },
+  }
 
-  unaryOpNode: function(node) {
-    var _this = this;
-    return _this.visit(node.right).then(function(rres) {
-      return _this.ctx.unaryop(node.op, rres.rv);
+  unaryOpNode(node) {
+    return this.visit(node.right).then((rres) => {
+      return this.ctx.unaryop(node.op, rres.rv);
     });
   }
-});
+}
 
-exports.create = function(root, ctx) {
+export function create(root, ctx) {
   return new SInterpreter(root, ctx);
-};
+}
