1a8f3ba89ff6a8930edb1e2abe0f4f7abb87ddf0
Frederick Polgardy <fred@polgardy.com>
2015-08-13 01:10:03 -0700

    blockly poc

--- web/main.js
+++ web/main.js
@@ -1,22 +1,13 @@
 import $ from 'jquery';
-import ace from 'brace';
-import 'brace/theme/textmate';
-import { default as StartMode } from './start_mode';
+
+// bizarre module definitions, but ok
+const Blockly = require('blockly')(window);
+require('blockly/lib/en_compressed.js')(null, Blockly);
+
 import { parse } from '../parser';
 import { createRuntime, globals, handle } from '../runtime';
 import { createInterpreter } from '../interpreter';
 import { paper } from '../graphics';
-import '../term';
-
-export const editor = ace.edit('editor');
-editor.setTheme('ace/theme/textmate');
-editor.setShowFoldWidgets(false);
-editor.getSession().setTabSize(2);
-editor.getSession().setUseSoftTabs(true);
-editor.getSession().setMode(new StartMode());
-
-export const terminal = $('#terminal').term({ prompt: '> ' });
-var termapi = terminal.data('term');
 
 function refresh() {
   return new Promise((resolve) => {
@@ -47,62 +38,32 @@ globals.clear = function() {
 
 // wire it up
 
-let ctx = createRuntime(),
-    session = editor.getSession(),
-    doc = session.getDocument(),
-    buffer = [],
-    level = 1;
+Blockly.inject($('#editor')[0], {
+  toolbox:
+    '<xml>' +
+      '<block type="logic_compare"></block>' +
+      '<block type="logic_operation"></block>' +
+      '<block type="logic_boolean"></block>' +
+    '</xml>'
+});
+
+let ctx = createRuntime();
 
 // hook up the run button
 $('#runner').click(() => {
   // wipe stuff clean and make a fresh program context
   ctx = createRuntime();
-  termapi.clear();
+  // termapi.clear();
   paper.clear();
   // execute the code in the buffer
   let root = parse(doc.getAllLines().join('\n')),
       interp = createInterpreter(root, ctx);
   interp.on('error', (err) => {
-    termapi.error('[ERROR]: ' + err.message);
-    termapi.focus();
+    // termapi.error('[ERROR]: ' + err.message);
+    // termapi.focus();
   });
   interp.on('end', () => {
-    termapi.focus();
+    // termapi.focus();
   });
   interp.run();
-});
-
-termapi.on('line', (command) => {
-  if (command) {
-    buffer.push(command);
-    // see if we're going into a nested block
-    if (/(?:do|then|else)\s*$/.test(command)) {
-      level++;
-      termapi.prompt = '>'.repeat(level) + ' ';
-    }
-    // see if we're exiting a nested block
-    if (/end\s*$/.test(command)) {
-      level--;
-      termapi.prompt = '>'.repeat(level) + ' ';
-    }
-    // if we're nested, don't evaluate
-    if (level > 1) {
-      return;
-    }
-    // execute the code in the buffer
-    let root = parse(buffer.join('\n') + '\n'),
-        interp = createInterpreter(root, ctx);
-    interp.on('error', (err) => {
-      termapi.error('[ERROR]: ' + err.message);
-      // reset the command buffer
-      buffer = [];
-    });
-    interp.on('end', () => {
-      // add command to the editor
-      doc.insertLines(doc.getLength() - 1, buffer);
-      // reset the command buffer
-      buffer = [];
-    });
-    interp.run();
-  }
 });
