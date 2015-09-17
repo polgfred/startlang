9b304a001c15a52efcbbc144f83a76ba2cdbf0b4
Frederick Polgardy <fred@polgardy.com>
2015-09-17 15:46:23 -0700

    a few more keywords for the syntax highlighter

--- brace_start/start_mode.js
+++ brace_start/start_mode.js
@@ -12,7 +12,7 @@ class StartHighlightRules extends TextHighlightRules {
       start: [
         {
           token: 'keyword',
-          regex: /\b(?:and|b(egin|reak|y)|do|e(lse|nd)|f(or|rom)|i[fn]|let|n(ext|ot)|or|t(hen|o)|return|w(hile|ith))\b/
+          regex: /\b(?:and|b(egin|reak|y)|count|do|e(lse|nd)|f(or|rom)|i[fn]|let|n(ext|ot)|or|t(hen|o)|return|w(hile|ith))\b/
         }, {
           token: 'constant.language',
           regex: /\b(?:false|infinity|none|true)\b/
@@ -21,7 +21,7 @@ class StartHighlightRules extends TextHighlightRules {
           regex: /\b(?:a(bs|cos|dd|sin|tan|vg)|c(eil|lamp|o(py|s))|diff|e(ndof|x(it|p))|f(irst|loor)|in(pu|ser)t|join|keys|l(ast|en|ist|o(g|wer))|m(ax|in)|num|p(art|ow|rint|ut)|r(and|e(mov|plac|vers)e|ound|sort)|s(huffle|in|leep|ort|plit|qrt|t(artof|r)|u(b|m)|wap)|t(a(ble|n)|ime)|upper)\b/
         }, {
           token: 'support.function',
-          regex: /\b(?:c(ircle|lone)|ellipse|fill|line|move|opacity|poly(gon|line)|r(e(ct|fresh|move)|otate)|s(cale|troke))\b/
+          regex: /\b(?:c(ircle|lone)|ellipse|fill|line|move|opacity|poly(gon|line)|r(e(ct|fresh|move|paint)|otate)|s(cale|troke))\b/
         }, {
           token: 'keyword.operator',
           regex: /[,+\-*/%!=<>&|~]/
