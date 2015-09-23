28c87809e6f2d3c7db306b0c915ff86e87bec2e1
Frederick Polgardy <fred@polgardy.com>
2015-09-23 10:19:12 -0700

    add rgb() to ace mode

--- brace_start/start_mode.js
+++ brace_start/start_mode.js
@@ -18,7 +18,7 @@ class StartHighlightRules extends TextHighlightRules {
           regex: /\b(?:false|infinity|none|true)\b/
         }, {
           token: 'support.function',
-          regex: /\b(?:a(bs|cos|dd|sin|tan|vg)|c(eil|lamp|o(py|s))|diff|e(ndof|x(it|p))|f(irst|loor)|in(pu|ser)t|join|keys|l(ast|en|ist|o(g|wer))|m(ax|in)|num|p(art|ow|rint|ut)|r(and|e(mov|plac|vers)e|ound|sort)|s(huffle|in|leep|ort|plit|qrt|t(artof|r)|u(b|m)|wap)|t(a(ble|n)|ime)|upper)\b/
+          regex: /\b(?:a(bs|cos|dd|sin|tan|vg)|c(eil|lamp|o(py|s))|diff|e(ndof|x(it|p))|f(irst|loor)|in(pu|ser)t|join|keys|l(ast|en|ist|o(g|wer))|m(ax|in)|num|p(art|ow|rint|ut)|r(and|e(mov|plac|vers)e|gb|ound|sort)|s(huffle|in|leep|ort|plit|qrt|t(artof|r)|u(b|m)|wap)|t(a(ble|n)|ime)|upper)\b/
         }, {
           token: 'support.function',
           regex: /\b(?:c(ircle|lone)|ellipse|fill|line|move|opacity|poly(gon|line)|r(e(ct|fresh|move|paint)|otate)|s(cale|troke))\b/
