86a40b74853c1cf8dd785cb735f6a223245f2624
Frederick Polgardy <fred@polgardy.com>
2015-09-23 10:09:16 -0700

    run in block or source mode based on param, persist workspace, disable run button while running

--- web/main.js
+++ web/main.js
@@ -13,72 +13,99 @@ import { createInterpreter } from '../lang/interpreter';
 import { createBuilder } from '../lang/builder';
 import { createRuntime } from '../lang/graphics';
 
-// Blockly.inject('editor', {
-//   toolbox: $('#toolbox')[0],
-//   collapse: true,
-//   comments: true,
-//   disable: true,
-//   readOnly: false,
-//   scrollbars: true,
-//   trashcan: true,
-//   grid: {
-//     spacing: 25,
-//     length: 3,
-//     colour: '#ccc',
-//     snap: true
-//   },
-//   zoom: {
-//     enabled: true,
-//     controls: true,
-//     wheel: true,
-//     maxScale: 2,
-//     minScale: .1,
-//     scaleSpeed: 1.1
-//   }
-// });
+let mode = (location.search.match('mode=(.*)') || [])[1];
+let getRoot;
 
-const editor = ace.edit('editor');
-editor.$blockScrolling = Infinity;
-editor.setTheme('ace/theme/textmate');
-editor.setShowFoldWidgets(false);
-editor.getSession().setTabSize(2);
-editor.getSession().setUseSoftTabs(true);
-editor.getSession().setMode(new StartMode());
+if (mode == 'blocks') {
+  Blockly.inject('editor', {
+    toolbox: $('#toolbox')[0],
+    collapse: true,
+    comments: true,
+    disable: true,
+    readOnly: false,
+    scrollbars: true,
+    trashcan: true,
+    grid: {
+      spacing: 25,
+      length: 3,
+      colour: '#ccc',
+      snap: true
+    },
+    zoom: {
+      enabled: true,
+      controls: true,
+      wheel: true,
+      maxScale: 2,
+      minScale: .1,
+      scaleSpeed: 1.1
+    }
+  });
 
-let ctx = createRuntime();
+  let ws = Blockly.getMainWorkspace();
+
+  getRoot = () => createBuilder().fromWorkspace(ws);
+
+  // make the workspace persist
+  $(window).on('load', () => {
+    let xml = localStorage['savedBlocks'] ?
+                Blockly.Xml.textToDom(localStorage['savedBlocks']) :
+                $('#new_workspace')[0];
+
+    Blockly.Xml.domToWorkspace(ws, xml);
+  });
+
+  $(window).on('unload', () => {
+    localStorage['savedBlocks'] = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(ws));
+  });
+} else {
+  let editor = ace.edit('editor');
+
+  editor.$blockScrolling = Infinity;
+  editor.setTheme('ace/theme/textmate');
+  editor.setShowFoldWidgets(false);
+  editor.getSession().setTabSize(2);
+  editor.getSession().setUseSoftTabs(true);
+  editor.getSession().setMode(new StartMode());
+
+  getRoot = () => parse(editor.getValue() + '\n');
+
+  // make the workspace persist
+  $(window).on('load', () => {
+    if (localStorage['savedSource']) {
+      editor.setValue(localStorage['savedSource'], -1);
+    }
+  });
+
+  $(window).on('unload', () => {
+    localStorage['savedSource'] = editor.getValue();
+  });
+}
+
+// initialize the display
+createRuntime();
 
 // hook up the run button
 $('#runner').click(() => {
   // wipe stuff clean and make a fresh program context
-  ctx = createRuntime();
   console.clear();
   // termapi.clear();
   // execute the code in the buffer
-  let //root = createBuilder().fromWorkspace(Blockly.getMainWorkspace()),
-      root = parse(editor.getSession().getDocument().getAllLines().join('\n')),
+  let root = getRoot(),
+      ctx = createRuntime(),
       interp = createInterpreter(root, ctx);
+
   console.log(root);
+
   interp.on('error', (err) => {
     console.error('[ERROR]: ' + err.message);
+    $('#runner').prop('disabled', false);
   });
+
   interp.on('end', () => {
     console.log('[END]');
+    $('#runner').prop('disabled', false);
   });
+
+  $('#runner').prop('disabled', true);
   interp.run();
 });
-
-// make the workspace persist
-
-// $(window).on('unload', function() {
-//   localStorage['save'] = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace()));
-// });
-//
-// window.restore = function() {
-//   let ws = Blockly.getMainWorkspace();
-//   ws.clear();
-//   Blockly.Xml.domToWorkspace(ws, Blockly.Xml.textToDom(localStorage['save']));
-// };
-//
-// $(window).on('load', function() {
-//   Blockly.Xml.domToWorkspace(Blockly.getMainWorkspace(), $('#new_workspace')[0]);
-// });
