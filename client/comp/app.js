9af278d96913fdaa169f9b2e0aad66d1751afe02
Frederick Polgardy <fred@polgardy.com>
2016-03-16 09:41:50 -0400

    start building out the ui with bootstrap

--- client/comp/app.js
+++ client/comp/app.js
@@ -3,7 +3,11 @@
 import { $ } from 'meteor/jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactBootstrap from 'react-bootstrap';
+
+import {
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Grid, Row, Col
+} from 'react-bootstrap';
 
 import immutable from 'immutable';
 
@@ -15,17 +19,30 @@ import CEditor from './editor';
 export default class CApp extends CBase {
   render() {
     return <div className="start-app">
-      <div className="start-display">
-        <div className="start-graphics">
-          <CGraphics data={{ shapes: immutable.List() }}/>
-        </div>
-        <div className="start-text">
-          <CTerm buf={ immutable.List() }/>
-        </div>
-      </div>
-      <div className="start-sidebar">
-        <CEditor />
-      </div>
+      <Navbar>
+        <Nav>
+          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={2} href="#">Link</NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3} href="#">Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+      <Grid className="start-body" fluid>
+        <Row>
+          <Col md={6}>
+            <CGraphics data={{ shapes: immutable.List() }}/>
+            <CTerm buf={ immutable.List() }/>
+          </Col>
+          <Col md={6}>
+            <CEditor />
+          </Col>
+        </Row>
+      </Grid>
     </div>;
   }
 }
--- client/comp/editor.js
+++ client/comp/editor.js
@@ -13,30 +13,17 @@ import CBase from './base';
 import StartMode from '../brace_start/start_mode';
 
 export default class CEditor extends CBase {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-  }
-
   render() {
-    return <div className="start-editor-wrapper">
-      <div className="start-editor"></div>
-      <button className="start-runner" onClick={ this.onClick }>Run</button>
-    </div>;
+    return <div className="start-editor" />;
   }
 
   componentDidMount() {
-    let editor = ace.edit(this.$('.start-editor')[0]);
+    let editor = ace.edit(this.$()[0]);
     editor.$blockScrolling = Infinity;
     editor.setTheme('ace/theme/textmate');
     editor.setShowFoldWidgets(false);
     editor.getSession().setTabSize(2);
     editor.getSession().setUseSoftTabs(true);
     editor.getSession().setMode(new StartMode());
-  }
-
-  onClick() {
-    let editor = ace.edit(this.$('.start-editor')[0]);
-    console.log(editor.getValue());
   }
 }
