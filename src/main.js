baeaf04baad7e539d3b3d62f1bd4652ea59fa548
Frederick Polgardy <fred@polgardy.com>
2018-05-20 21:07:53 -0600

    don't import the main stylesheet yet

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './ui/app';
 
-import './main.css';
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('start-wrapper'));
 }, false);
