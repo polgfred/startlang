5347fcdf8feb8aee497c497c5215aa3f774dc929
Frederick Polgardy <fred@polgardy.com>
2019-08-18 15:27:16 -0600

    bring in postcss-mixins and freshen up (and actually import) the stylesheet

--- postcss.config.js
+++ postcss.config.js
@@ -9,5 +9,6 @@ module.exports = {
         'color-mod-function': true,
       },
     },
+    'postcss-mixins': {},
   },
 };
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './ui/app';
 
+import './main.css';
+
 document.addEventListener(
   'DOMContentLoaded',
   () => {
