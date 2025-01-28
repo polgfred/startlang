c131506e221785aa233c149457f42d87e8db7aca
Frederick Polgardy <fred@polgardy.com>
2025-01-28 05:58:23 +0000

    clean up shape generation

--- app/home/graphics.jsx
+++ app/home/graphics.jsx
@@ -1,98 +1,103 @@
 'use client';
 
-import { createElement } from 'react';
-
 export default function Graphics({ shapes }) {
   return (
     <svg
-      className="start-graphics"
       sx={{
         height: '100%',
         width: '100%',
       }}
     >
-      <g className="start-orient">
-        {shapes.map((elem, index) =>
-          createElement(registry[elem.type], {
-            key: index,
-            shape: elem,
-          })
-        )}
+      <g>
+        {shapes.map((shape, index) => {
+          const { sprops, tprops } = shape;
+          const styles = { style: {} };
+          let trans = '';
+
+          if (sprops.stroke) {
+            styles.style.stroke = sprops.stroke;
+          }
+          if (sprops.fill) {
+            styles.style.fill = sprops.fill;
+          }
+          if (sprops.opacity >= 0 && sprops.opacity < 1) {
+            styles.style.opacity = sprops.opacity;
+          }
+          if (sprops.rotate !== 0) {
+            trans += `rotate(${sprops.rotate}deg)`;
+          }
+          if (sprops.scalex !== 1 || sprops.scaley !== 1) {
+            trans += `scale(${sprops.scalex},${sprops.scaley})`;
+          }
+          if (trans) {
+            styles.style.transform = trans;
+            styles.style.transformOrigin = sprops.anchor;
+          }
+          if (tprops) {
+            styles.fontFamily = tprops.fface;
+            styles.fontSize = tprops.fsize;
+          }
+
+          switch (shape.type) {
+            case 'rect': {
+              const { x, y, width, height } = shape;
+              return (
+                <rect
+                  key={index}
+                  x={x}
+                  y={y}
+                  width={width}
+                  height={height}
+                  {...styles}
+                />
+              );
+            }
+            case 'circle': {
+              const { cx, cy, r } = shape;
+              return <circle key={index} cx={cx} cy={cy} r={r} {...styles} />;
+            }
+            case 'ellipse': {
+              const { cx, cy, rx, ry } = shape;
+              return (
+                <ellipse
+                  key={index}
+                  cx={cx}
+                  cy={cy}
+                  rx={rx}
+                  ry={ry}
+                  {...styles}
+                />
+              );
+            }
+            case 'line': {
+              const { x1, y1, x2, y2 } = shape;
+              return (
+                <line key={index} x1={x1} y1={y1} x2={x2} y2={y2} {...styles} />
+              );
+            }
+            case 'polygon': {
+              const { points } = shape;
+              return (
+                <polygon key={index} points={points.join(',')} {...styles} />
+              );
+            }
+            case 'text': {
+              const { x, y, value } = shape;
+              return (
+                <text
+                  key={index}
+                  x={x}
+                  y={y}
+                  textAnchor={tprops.align}
+                  {...styles}
+                >
+                  {value}
+                </text>
+              );
+            }
+          }
+        })}
       </g>
     </svg>
   );
 }
-
-function getStyles(sprops, tprops) {
-  let attrs = { style: {} };
-  let trans = '';
-
-  if (sprops.stroke) {
-    attrs.style.stroke = sprops.stroke;
-  }
-  if (sprops.fill) {
-    attrs.style.fill = sprops.fill;
-  }
-  if (sprops.opacity >= 0 && sprops.opacity < 1) {
-    attrs.style.opacity = sprops.opacity;
-  }
-  if (sprops.rotate !== 0) {
-    trans += `rotate(${sprops.rotate}deg)`;
-  }
-  if (sprops.scalex !== 1 || sprops.scaley !== 1) {
-    trans += `scale(${sprops.scalex},${sprops.scaley})`;
-  }
-  if (trans) {
-    attrs.style.transform = trans;
-    attrs.style.transformOrigin = sprops.anchor;
-  }
-  if (tprops) {
-    attrs.fontFamily = tprops.fface;
-    attrs.fontSize = tprops.fsize;
-  }
-
-  return attrs;
-}
-
-function Rect({ shape: { sprops, x, y, width, height } }) {
-  const styles = getStyles(sprops);
-  return <rect {...styles} x={x} y={y} width={width} height={height} />;
-}
-
-function Circle({ shape: { sprops, cx, cy, r } }) {
-  const styles = getStyles(sprops);
-  return <circle {...styles} cx={cx} cy={cy} r={r} />;
-}
-
-function Ellipse({ shape: { sprops, cx, cy, rx, ry } }) {
-  const styles = getStyles(sprops);
-  return <ellipse {...styles} cx={cx} cy={cy} rx={rx} ry={ry} />;
-}
-
-function Line({ shape: { sprops, x1, y1, x2, y2 } }) {
-  const styles = getStyles(sprops);
-  return <line {...styles} x1={x1} y1={y1} x2={x2} y2={y2} />;
-}
-
-function Polygon({ shape: { sprops, points } }) {
-  const styles = getStyles(sprops);
-  return <polygon {...styles} points={points.join(',')} />;
-}
-
-function Text({ shape: { sprops, tprops, x, y, value } }) {
-  const styles = getStyles(sprops, tprops);
-  return (
-    <text {...styles} x={x} y={y} textAnchor={tprops.align}>
-      {value}
-    </text>
-  );
-}
-
-const registry = {
-  rect: Rect,
-  circle: Circle,
-  ellipse: Ellipse,
-  line: Line,
-  polygon: Polygon,
-  text: Text,
-};
