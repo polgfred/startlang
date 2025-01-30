1e14ce418342965ce1acbd462221a6365d661f64
Frederick Polgardy <fred@polgardy.com>
2025-01-30 06:43:05 +0000

    syntax highlighting!

--- app/home/editor.jsx
+++ app/home/editor.jsx
@@ -7,6 +7,66 @@ import { parse } from '../../src/lang/parser.peggy';
 import boxScript from '../../tests/box.start';
 
 export default function Editor({ setParser }) {
+  const onBeforeMount = useCallback((monaco) => {
+    monaco.languages.register({ id: 'start' });
+    monaco.languages.setMonarchTokensProvider('start', {
+      defaultToken: '',
+      ignoreCase: true,
+      keywords: [
+        'and',
+        'begin',
+        'break',
+        'by',
+        'do',
+        'else',
+        'end',
+        'exit',
+        'for',
+        'from',
+        'if',
+        'in',
+        'let',
+        'local',
+        'next',
+        'not',
+        'or',
+        'repeat',
+        'return',
+        'then',
+        'to',
+        'while',
+      ],
+      operators: ['+', '-', '*', '/', '%', '!', '<', '<=', '=', '>', '>='],
+      tokenizer: {
+        root: [
+          [
+            /[a-zA-Z_][\w]*/,
+            {
+              cases: {
+                '@keywords': 'keyword',
+                '@default': 'identifier',
+              },
+            },
+          ],
+          [/[ \t\r\n]+/, 'white'],
+          [/;.*$/, 'comment'],
+          [/[,+\-*/%!=<>&|~]/, 'delimiter'],
+          [/\d+\.\d+([eE][-+]?\d+)?/, 'number.float'],
+          [/\d+/, 'number'],
+          [/"/, 'string', '@string'],
+        ],
+        string: [
+          [/""/, 'string'],
+          [/``/, 'string'],
+          [/`/, { token: 'string', next: '@interp' }],
+          [/[^"`]+/, 'string'],
+          [/"/, 'string', '@pop'],
+        ],
+        interp: [[/`/, 'string', '@pop'], { include: 'root' }],
+      },
+    });
+  }, []);
+
   const onEditorMount = useCallback(
     (editor) => {
       editor.focus();
@@ -23,6 +83,8 @@ export default function Editor({ setParser }) {
   return (
     <Monaco
       defaultValue={boxScript}
+      language="start"
+      beforeMount={onBeforeMount}
       onMount={onEditorMount}
       options={{
         minimap: { enabled: false },
