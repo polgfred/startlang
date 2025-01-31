7f0fcebae580d903d5feac7419b15f2990b5bf4f
Frederick Polgardy <fred@polgardy.com>
2025-01-31 00:08:15 +0000

    finally fix rotation/scale bug

--- app/home/graphics.jsx
+++ app/home/graphics.jsx
@@ -1,5 +1,104 @@
 'use client';
 
+function Shape({ shape }) {
+  const {
+    sprops: { rotate, scalex, scaley, anchor, stroke, fill, opacity },
+    tprops: { fface, fsize, align },
+  } = shape;
+  const styles = {
+    style: {},
+  };
+  const transforms = [];
+
+  if (rotate !== 0) {
+    transforms.push(`rotate(${rotate})`);
+  }
+  if (scalex !== 1 || scaley !== 1) {
+    transforms.push(`scale(${scalex} ${scaley})`);
+  }
+  if (anchor) {
+    styles.style.transformOrigin = anchor;
+    styles.style.transformBox = 'fill-box';
+  }
+  if (stroke) {
+    styles.style.stroke = stroke;
+  }
+  if (fill) {
+    styles.style.fill = fill;
+  }
+  if (opacity >= 0 && opacity < 1) {
+    styles.style.opacity = opacity;
+  }
+  if (fface) {
+    styles.fontFamily = fface;
+  }
+  if (fsize) {
+    styles.fontSize = fsize;
+  }
+
+  const transform = transforms.join(' ');
+
+  switch (shape.type) {
+    case 'rect': {
+      const { x, y, width, height } = shape;
+      return (
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          transform={transform}
+          {...styles}
+        />
+      );
+    }
+    case 'circle': {
+      const { cx, cy, r } = shape;
+      return <circle cx={cx} cy={cy} r={r} transform={transform} {...styles} />;
+    }
+    case 'ellipse': {
+      const { cx, cy, rx, ry } = shape;
+      return (
+        <ellipse
+          cx={cx}
+          cy={cy}
+          rx={rx}
+          ry={ry}
+          transform={transform}
+          {...styles}
+        />
+      );
+    }
+    case 'line': {
+      const { x1, y1, x2, y2 } = shape;
+      return (
+        <line
+          x1={x1}
+          y1={y1}
+          x2={x2}
+          y2={y2}
+          transform={transform}
+          {...styles}
+        />
+      );
+    }
+    case 'polygon': {
+      const { points } = shape;
+      return (
+        <polygon points={points.join(',')} transform={transform} {...styles} />
+      );
+    }
+    case 'text': {
+      const { x, y, value } = shape;
+      return (
+        <text x={x} y={y} textAnchor={align} transform={transform} {...styles}>
+          {value}
+        </text>
+      );
+    }
+  }
+}
+
 export default function Graphics({ shapes }) {
   return (
     <svg
@@ -9,94 +108,9 @@ export default function Graphics({ shapes }) {
       }}
     >
       <g>
-        {shapes.map((shape, index) => {
-          const { sprops, tprops } = shape;
-          const styles = { style: {} };
-          let trans = '';
-
-          if (sprops.stroke) {
-            styles.style.stroke = sprops.stroke;
-          }
-          if (sprops.fill) {
-            styles.style.fill = sprops.fill;
-          }
-          if (sprops.opacity >= 0 && sprops.opacity < 1) {
-            styles.style.opacity = sprops.opacity;
-          }
-          if (sprops.rotate !== 0) {
-            trans += `rotate(${sprops.rotate}deg)`;
-          }
-          if (sprops.scalex !== 1 || sprops.scaley !== 1) {
-            trans += `scale(${sprops.scalex},${sprops.scaley})`;
-          }
-          if (trans) {
-            styles.style.transform = trans;
-            styles.style.transformOrigin = sprops.anchor;
-          }
-          if (tprops) {
-            styles.fontFamily = tprops.fface;
-            styles.fontSize = tprops.fsize;
-          }
-
-          switch (shape.type) {
-            case 'rect': {
-              const { x, y, width, height } = shape;
-              return (
-                <rect
-                  key={index}
-                  x={x}
-                  y={y}
-                  width={width}
-                  height={height}
-                  {...styles}
-                />
-              );
-            }
-            case 'circle': {
-              const { cx, cy, r } = shape;
-              return <circle key={index} cx={cx} cy={cy} r={r} {...styles} />;
-            }
-            case 'ellipse': {
-              const { cx, cy, rx, ry } = shape;
-              return (
-                <ellipse
-                  key={index}
-                  cx={cx}
-                  cy={cy}
-                  rx={rx}
-                  ry={ry}
-                  {...styles}
-                />
-              );
-            }
-            case 'line': {
-              const { x1, y1, x2, y2 } = shape;
-              return (
-                <line key={index} x1={x1} y1={y1} x2={x2} y2={y2} {...styles} />
-              );
-            }
-            case 'polygon': {
-              const { points } = shape;
-              return (
-                <polygon key={index} points={points.join(',')} {...styles} />
-              );
-            }
-            case 'text': {
-              const { x, y, value } = shape;
-              return (
-                <text
-                  key={index}
-                  x={x}
-                  y={y}
-                  textAnchor={tprops.align}
-                  {...styles}
-                >
-                  {value}
-                </text>
-              );
-            }
-          }
-        })}
+        {shapes.map((shape, index) => (
+          <Shape key={index} shape={shape} />
+        ))}
       </g>
     </svg>
   );
